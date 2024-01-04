f2758f5ea8e445b09354075c9074c6e8bd0af252
kichaudh <djkiran93@gmail.com>
2024-01-04 18:11:10 +0530

    code for subtraction of two no

--- addition.py
+++ addition.py
@@ -2,7 +2,7 @@ def add (a,b):
     ans = a+b
     return ans
 
-x= 1000
-y= 2000
+x = 1000
+y = 2000
 ans = add (x,y)
 print (f"addition of {x} and {y} is {ans}")
---
+++ subtraction.py
@@ -0,0 +1,8 @@
+def add (a,b):
+    ans = a-b
+    return ans
+
+x = 6000
+y = 2000
+ans = add (x,y)
+print (f"subtraction of {x} and {y} is {ans}")
